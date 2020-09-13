633616c557b6c74e4dde3f5f50cad8e975a61de0
Jihwan Kim <jkim3157@gatech.edu>
2020-09-13 10:42:09 -0400

    added the character movement
    added basic movement and jump for player character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private int grounded;
+
+    public bool IsGrounded
+    {
+        get
+        {
+            return grounded > 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        grounded = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        {
+            Vector3 movement = new Vector3(0.0f, 1500 * Time.deltaTime, 0.0f);
+            rb.AddForce(movement * speed);
+        }
+
+        transform.position += new Vector3(moveHorizontal * Time.deltaTime * speed, 0, moveVertical * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            grounded++;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            grounded--;
+        }
+    }
+
+}
