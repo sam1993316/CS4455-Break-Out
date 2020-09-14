89e125b416609ae5d7c1755458bfab2f27cb4994
sam1993316 <54906199+sam1993316@users.noreply.github.com>
2020-09-13 21:24:17 -0400

    added camera switching
    pretty T to detach/reattach and switch camera view

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 offset;
+    public GameObject oldTarget;
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+        oldTarget = target;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (oldTarget != target)
+        {
+            offset = transform.position - target.transform.position;
+            oldTarget = target;
+        }
+        transform.position = target.transform.position + offset;
+
+
+    }
+
+}
