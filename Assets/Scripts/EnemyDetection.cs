b42a1c39102d242ad451c69f52f0a6c01937d29e
Jimmy Pham <jpham197@gmail.com>
2020-10-07 16:51:28 -0400

    Implement player detection for zombie model, kind of buggy atm tho but works OK

--- Assets/Scripts/EnemyDetection.cs
+++ Assets/Scripts/EnemyDetection.cs
@@ -1,65 +1,78 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using TMPro;
-using UnityEngine.XR;
 
 public class EnemyDetection : MonoBehaviour
 {
+    public GameObject eyes;
     public GameObject player;
-    public GameObject enemyEyes;
     public TextMeshProUGUI detectionText;
-    public Rigidbody body;
 
-    float FOVAngle = 20;
+    float maxFOVAngle = 20;
     float lookRadius = 8f;
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 fovRadius = enemyEyes.gameObject.transform.forward * lookRadius;
-        float playerAngle = Vector3.Angle(player.gameObject.transform.position - enemyEyes.gameObject.transform.position, fovRadius);
+        Vector3 fovRadius = eyes.gameObject.transform.forward * lookRadius;
+        float playerAngle = Vector3.Angle(player.gameObject.transform.position - eyes.gameObject.transform.position, fovRadius);
 
-        if (playerAngle < FOVAngle)
+        
+
+        if (playerAngle < maxFOVAngle)
         {
+
             RaycastHit hit;
 
-            Vector3 newPlayerDirection = player.gameObject.transform.position - enemyEyes.gameObject.transform.position;
-            Debug.DrawRay(enemyEyes.gameObject.transform.position, newPlayerDirection);
-            if (Physics.Raycast(enemyEyes.gameObject.transform.position, newPlayerDirection, out hit))
+            // player - enemy's eyes position for direction, bc just player will use position from origin to player instead of enemy eye to player
+            Vector3 newPlayerDirection = player.gameObject.transform.position - eyes.gameObject.transform.position;
+            Debug.DrawRay(eyes.gameObject.transform.position, newPlayerDirection);
+            if (Physics.Raycast(eyes.gameObject.transform.position, newPlayerDirection, out hit))
             {
                 if (hit.collider.CompareTag("Player"))
                 {
+                    Debug.Log("HIT");
                     detectionText.text = "Player detected";
+                    moveTowardsPlayer();
                 }
                 else
                 {
-                    detectionText.text = "Something other than player detected";
+                    Debug.Log("no player hit");
+                    detectionText.text = "Player NOT detected";
                 }
             }
             else
             {
-                detectionText.text = "Nothing detected";
+                Debug.Log("nothing hit");
+                detectionText.text = "NOTHING detected";
             }
         }
-
     }
 
     void moveTowardsPlayer()
     {
-        float speed = 1.0f;
+        float speed = 5.0f;
         float distanceToStop = 5.0f;
+        Vector3 position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.fixedDeltaTime);
+
         if (Vector3.Distance(transform.position, player.transform.position) > distanceToStop)
         {
             transform.LookAt(player.transform);
-            body.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+            //rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+            rb.MovePosition(position);
+
         }
     }
 }
