cfc22c11339c9c8b4e0ee951a1d00828af9fee5d
Jimmy Pham <jpham197@gmail.com>
2020-10-07 00:31:42 -0400

    Improve moveTowardsPlayer(), no longer falling over

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -10,7 +10,7 @@ public class EnemyFOV : MonoBehaviour
     public GameObject player;
     public GameObject test;
     public TextMeshProUGUI detectionText;
-    float maxFOVAngle = 30;
+    float maxFOVAngle = 20;
     float lookRadius = 8f;
 
     private Rigidbody rb;
@@ -33,11 +33,11 @@ public class EnemyFOV : MonoBehaviour
         {
             
             RaycastHit hit;
-            // player - enemy's eyes position for direction
-            
-            Vector3 newDirection = player.gameObject.transform.position - eyes.gameObject.transform.position;
-            Debug.DrawRay(eyes.gameObject.transform.position, newDirection);
-            if (Physics.Raycast(eyes.gameObject.transform.position, newDirection, out hit))
+
+            // player - enemy's eyes position for direction, bc just player will use position from origin to player instead of enemy eye to player
+            Vector3 newPlayerDirection = player.gameObject.transform.position - eyes.gameObject.transform.position;
+            Debug.DrawRay(eyes.gameObject.transform.position, newPlayerDirection);
+            if (Physics.Raycast(eyes.gameObject.transform.position, newPlayerDirection, out hit))
             {
                 if (hit.collider.CompareTag("Player"))
                 {
@@ -63,46 +63,14 @@ public class EnemyFOV : MonoBehaviour
     {
         float speed = 5.0f;
         float distanceToStop = 5.0f;
+        Vector3 position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.fixedDeltaTime);
+
         if (Vector3.Distance(transform.position, player.transform.position) > distanceToStop)
         {
             transform.LookAt(player.transform);
-            rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
-        }
-        if (Vector3.Distance(transform.position, player.transform.position) < distanceToStop && Vector3.Distance(transform.position, player.transform.position) > 1f) {
-            rb.AddRelativeForce(Vector3.back * speed, ForceMode.Force);
+            //rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+            rb.MovePosition(position);
+            
         }
     }
 }
-
-/*
-
-Vector3 fovRadius = eyes.gameObject.transform.forward * lookRadius;
-        float playerAngle = Vector3.Angle(player.gameObject.transform.position - eyes.gameObject.transform.position, fovRadius);
-
-        
-        if (playerAngle < maxFOVAngle)
-        {
-            Debug.DrawRay(eyes.gameObject.transform.position, player.gameObject.transform.position);
-            RaycastHit hit;
-            if (Physics.Raycast(eyes.gameObject.transform.position, player.gameObject.transform.position, out hit))
-            {
-                if (hit.collider.CompareTag("Player"))
-                {
-                    Debug.Log("HIT");
-                    detectionText.text = "Player detected";
-                    moveTowardsPlayer();
-                }
-                else
-                {
-                    Debug.Log("no player hit");
-                    detectionText.text = "Player NOT detected";
-                }
-            }
-            else
-            {
-                Debug.Log("nothing hit");
-                detectionText.text = "NOTHING detected";
-            }
-        }
-
-*/
