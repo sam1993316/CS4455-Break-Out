896b7a8d1605d7252764b87f020a9c120dc1a499
Jihwan Kim <jkim3157@gatech.edu>
2020-10-15 00:12:10 -0400

    added the ingame menu

---
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuToggle : MonoBehaviour
+{
+
+    private CanvasGroup canvasGroup;
+    private GameObject mainMenu;
+    private GameObject optionsMenu;
+
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (canvasGroup == null)
+            Debug.LogError("CanvasGroup could not be found");
+
+        mainMenu = GameObject.Find("Main Menu");
+        if (mainMenu == null)
+            Debug.LogError("Main Menu could not be found");
+
+        optionsMenu = GameObject.Find("Options Menu");
+        if (optionsMenu == null)
+            Debug.LogError("Options Menu could not be found");
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (canvasGroup.interactable)
+            {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.alpha = 0f;
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.alpha = 1f;
+                Time.timeScale = 0f;
+                mainMenu.SetActive(true);
+                optionsMenu.SetActive(false);
+            }
+        }
+    }
+}
