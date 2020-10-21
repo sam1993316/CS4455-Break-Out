d3052d256c5fcda838b08c429946d4b24fd24dc2
Jihwan Kim <jkim3157@gatech.edu>
2020-10-21 14:31:44 -0400

    added player footstep sound. Need to fix making more step sounds after the player stops moving.

---
+++ Assets/Scripts/PlayerFootstepSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip[] clips;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.Log("Audio Source cannot be found.");
+        }
+    }
+
+    void Step()
+    {
+        //AudioClip clip = GetRandomClip();
+        AudioClip clip = clips[0];
+        audioSource.PlayOneShot(clip);
+    }
+
+    //AudioClip GetRandomClip()
+    //{
+    //    return clips[UnityEngine.Random.Range(0, clips.Length)];
+    //}
+}
