bed80102a2c8a1e50730b55e25297825c67dcc38
ThisnotNoah <The.Noah.Le@gmail.com>
2020-10-25 13:13:34 -0400

    Added Test Door and Scripts

---
+++ Assets/Scripts/KeyCardDoor/DoorAnimatorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimatorController : MonoBehaviour
+{
+    public string thisDoorsKey;
+    public GameObject player;
+    private KeyCardCollector kc;
+
+
+    // [SerializeField] private Animator slideDoorController;
+    private void OnTriggerEnter(Collider c)
+    {
+        // inlcude a check here for a keycard
+        kc = player.GetComponent<KeyCardCollector>();
+        if (c.CompareTag("Player"))
+        {
+            Debug.Log("Player has entered Door Collider");
+            // slideDoorController.SetBool("slide", true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.CompareTag("Player"))
+        {
+            Debug.Log("Player has exited the Door Collider");
+            // slideDoorController.SetBool("slide", false);
+        }
+    }
+}
---
+++ Assets/Scripts/KeyCardDoor/KeyCardCollector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardCollector : MonoBehaviour
+{
+    // this class will tell us if the player has the various different keycards available in the level
+        // setting the keyIDs to public will allow us to assign a key to one of these
+    public bool hasYellowKey;
+    public string YellowKeyID;
+
+    public bool hasBlueKey;
+    public string BlueKeyID; 
+
+    void Start()
+    {
+        hasYellowKey = false;
+        hasBlueKey = false;
+    }
+
+    public void ReceiveKey(string keyID)
+    {
+        if (keyID == YellowKeyID) {
+            hasYellowKey = true;
+        }
+        if (keyID == BlueKeyID) {
+            hasBlueKey = true;
+        }
+    }
+}
