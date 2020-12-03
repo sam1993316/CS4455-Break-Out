a223a0f07bea3013eb1ea5e2f7ba98136995a7bf
Jihwan Kim <jkim3157@gatech.edu>
2020-12-03 16:33:47 -0500

    updated some objective tasks

--- Assets/Scripts/ObjectiveTasks.cs
+++ Assets/Scripts/ObjectiveTasks.cs
@@ -9,11 +9,23 @@ public class ObjectiveTasks : MonoBehaviour
     public Text tasksText;
     public string currentTask;
 
+    private Collider m_ObjectCollider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ObjectCollider = GetComponent<Collider>();
+        m_ObjectCollider.isTrigger = true;
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             tasksText.text = currentTask;
+            m_ObjectCollider.isTrigger = false;
         }
     }
 }
