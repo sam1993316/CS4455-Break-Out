30a29ab6db8758b337708f06b3bac81a94baec54
sam1993316 <54906199+sam1993316@users.noreply.github.com>
2020-12-04 17:41:18 -0500

    aiming system added
    -only on level0 for now
    -probably still needs tweaking

---
+++ Assets/Scripts/Aiming.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class Aiming : MonoBehaviour
+{
+
+    public CinemachineFreeLook thirdPersonCamera;
+    public GameObject crosshair;
+    public Transform aimLookAt;
+    public Transform defaultLookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (thirdPersonCamera.m_Lens.FieldOfView == 40)
+            {
+                thirdPersonCamera.m_Lens.FieldOfView = 20;
+                thirdPersonCamera.LookAt = aimLookAt;
+                crosshair.SetActive(true);
+            }
+            else
+            {
+                thirdPersonCamera.m_Lens.FieldOfView = 40;
+                thirdPersonCamera.LookAt = defaultLookAt;
+                crosshair.SetActive(false);
+            }
+        }
+    }
+}
