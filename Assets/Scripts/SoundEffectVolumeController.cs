3db985f53880385f6860c8e5a806e89c2e9f8e75
Jihwan Kim <jkim3157@gatech.edu>
2020-12-05 16:38:33 -0500

    added volume slider to option menu and added the ending task texts

---
+++ Assets/Scripts/SoundEffectVolumeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectVolumeController : MonoBehaviour
+{
+    private AudioSource soundEffect;
+    private float volume = 0.5f;
+
+    void Start()
+    {
+        soundEffect = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        soundEffect.volume = volume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        volume = vol;
+    }
+}
